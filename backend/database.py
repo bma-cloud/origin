e33f0f906e36438c7f8a52488c5a96bdea8c2188
ia <ia@ivebat.local>
2026-04-02 12:09:18 +0200

    debug et installation des dépendances nécessaires pour le lancement du projet

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,8 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+
+load_dotenv()
 
 MONGO_URL = os.environ.get("MONGO_URL")
 DB_NAME = os.environ.get("DB_NAME")
